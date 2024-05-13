a821b345b95a206d219dac8ad0ae68a6f00ea2d3
SagarMakhar <sagarmakhar@gmail.com>
2024-05-13 22:55:58 +0000

    Carnival: UDFPS animations
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- src/com/clown/settings/fragments/themes/Themes.java
+++ src/com/clown/settings/fragments/themes/Themes.java
@@ -9,6 +9,7 @@ package com.clown.settings.fragments.themes;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
@@ -17,6 +18,7 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.clown.Utils;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -34,9 +36,13 @@ public class Themes extends SettingsPreferenceFragment implements
 
     private static final String KEY_ICONS_CATEGORY = "themes_icons_category";
     private static final String KEY_SIGNAL_ICON = "android.theme.customization.signal_icon";
+    private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
+    private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
 
     private PreferenceCategory mIconsCategory;
     private Preference mSignalIcon;
+    private PreferenceCategory mAnimationsCategory;
+    private Preference mUdfpsAnimation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,9 +56,22 @@ public class Themes extends SettingsPreferenceFragment implements
 
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
         mSignalIcon = (Preference) findPreference(KEY_SIGNAL_ICON);
+        mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
+        mUdfpsAnimation = (Preference) findPreference(KEY_UDFPS_ANIMATION);
 
         if (!DeviceUtils.deviceSupportsMobileData(context)) {
             mIconsCategory.removePreference(mSignalIcon);
+        }
+
+        FingerprintManager fingerprintManager = (FingerprintManager)
+                getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+
+        if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+            mAnimationsCategory.removePreference(mUdfpsAnimation);
+        } else {
+            if (!Utils.isPackageInstalled(context, "com.clown.udfps.animations")) {
+                mAnimationsCategory.removePreference(mUdfpsAnimation);
+            }
         }
     }
 
@@ -76,8 +95,19 @@ public class Themes extends SettingsPreferenceFragment implements
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
 
+                FingerprintManager fingerprintManager = (FingerprintManager)
+                        context.getSystemService(Context.FINGERPRINT_SERVICE);
+
                 if (!DeviceUtils.deviceSupportsMobileData(context)) {
                     keys.add(KEY_SIGNAL_ICON);
+                }
+
+                if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+                    keys.add(KEY_UDFPS_ANIMATION);
+                } else {
+                    if (!Utils.isPackageInstalled(context, "com.clown.udfps.animations")) {
+                        keys.add(KEY_UDFPS_ANIMATION);
+                    }
                 }
                 return keys;
             }
